ac2c8d54118cdc956d3f1f5d51d7dd7336155bf3
Oleg Ten <tennet0505@gmail.com>
2024-01-04 19:42:52 +0100

    fix merge conflicts

--- lib/rails_loki_exporter_dev/intercepting_logger.rb
+++ lib/rails_loki_exporter_dev/intercepting_logger.rb
@@ -21,17 +21,6 @@ module RailsLokiExporterDev
 
       severity_name = severity_name(severity)
       log_message = message || (block&.call)
-      
-      # puts ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-      # puts "message: #{message}"
-      # puts "(block&.call): #{(block&.call)}"
-      # puts "log_message: #{log_message}"
-      # @log = format_message(severity_name, Time.now, progname, log_message)
-      # if @intercept_logs
-      #   client.send_log(@log) if client
-      #   puts "@log: #{@log}"
-      #   puts "<<<<<<<<<<<<<<<<<<<<<<<<"
-      # end
 
       if @intercept_logs
         if message.nil?
